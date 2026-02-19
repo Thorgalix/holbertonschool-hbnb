7e8ce7f39502c0542a2f93843c3b4ac2fd0f2ee1
mahmoud BOUATE <12065@holbertonschool.com>
2026-02-19 12:19:36 +0100

    Add user registration and retrieval endpoints with input validation

--- part2/hbnb/app/models/user.py
+++ part2/hbnb/app/models/user.py
@@ -1,13 +1,15 @@
-from Base_Class import BaseClass
+from app.models.Base_Class import BaseClass
 
 class User(BaseClass):
-	def __init__(self, email, first_name, last_name, password, is_admin=False):
+	def __init__(self, email, first_name, last_name, password,user_id, is_admin=False):
 		super().__init__()
 		self.email = email
 		self.first_name = first_name
 		self.last_name = last_name
 		self.password = password
 		self.is_admin = is_admin
+		self.user_id = user_id
+
 
 		self.places = []
 		self.reviews = []

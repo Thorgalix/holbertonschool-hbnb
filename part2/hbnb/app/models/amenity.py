aa2e46d871348f623a79cdb4d4e737a372b50eec
mahmoud BOUATE <12065@holbertonschool.com>
2026-03-05 10:13:27 +0100

    Ajouter la validation du nom dans le modèle d'amenité pour garantir qu'il ne soit pas vide et qu'il ne dépasse pas 50 caractères.

--- part2/hbnb/app/models/amenity.py
+++ part2/hbnb/app/models/amenity.py
@@ -5,3 +5,18 @@ class Amenity(BaseClass):
         super().__init__()
         self.name = name
         self.places = []
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        self._name = self.validate_name(value)
+
+    def validate_name(self, name):
+        if name == "":
+            raise ValueError("Name should be not empty")
+        if len(name) > 50:
+            raise ValueError("Name must be maximum 50 characters long")
+        return name
